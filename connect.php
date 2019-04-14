8f8fc2752d2ac145d8635bbfc267384bbf61ac88
msaleh7 <49427869+msaleh7@users.noreply.github.com>
2019-04-13 21:27:14 -0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -13,7 +13,11 @@ $conn = new mysqli();
 $conn->init();
 
 // below contains certificate path required in azure
-$conn->ssl_set(NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); // set SSL options
+//$conn->ssl_set(NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); // set SSL options //AES256-SHA
+
+$conn->ssl_set(NULL,NULL, "AES256-SHA", NULL, NULL); // set SSL options //AES256-SHA
+
+
 
 $conn->real_connect('mysqldb7.mysql.database.azure.com', 'msaleh7', 'Msm@9810959', 'mysqldb7', 3306, MYSQLI_CLIENT_SSL); // actually establish connection via port 3306 using ssl
 
