e07dc79a39be6eff451b821f564670260a9e6176
msaleh7 <49427869+msaleh7@users.noreply.github.com>
2019-04-13 21:19:59 -0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -6,22 +6,16 @@
 
 
 
-$serverName = "mysqldb7.mysql.database.azure.com"; // update me
-$connectionOptions = array(
-"Database" => "blogdb", // db name
-"Uid" => "msaleh7@mysqldb7", // update me
-"PWD" => "Msm@9810959" // update me
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
 
-//$conn = new mysqli();
-//$conn->init();
+
+
+$conn = new mysqli();
+$conn->init();
 
 // below contains certificate path required in azure
-//$conn->ssl_set(NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); // set SSL options
+$conn->ssl_set(NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); // set SSL options
 
-//$conn->real_connect('mysqldb7.mysql.database.azure.com', 'msaleh@mysqldb7', 'msaleh123', 'mysqldb7', 3306, MYSQLI_CLIENT_SSL); // actually establish connection via port 3306 using ssl
+$conn->real_connect('mysqldb7.mysql.database.azure.com', 'msaleh@mysqldb7', 'Msm@9810959', 'mysqldb7', 3306, MYSQLI_CLIENT_SSL); // actually establish connection via port 3306 using ssl
 
 if ($conn->connect_error) {
     die ("Could not connect. ".$conn->connect_error);
